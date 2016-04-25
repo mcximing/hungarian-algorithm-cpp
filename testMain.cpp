b06caa5e3598a64f504ef4dc7dc9d0f10f7c9efc
mcximing <mcximing@sina.cn>
2016-04-25 21:49:20 +0800

    Init commit

---
+++ testMain.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Hungarian.h"
+
+
+int main(void)
+{
+	vector<vector<double>> costMatrix = { { 10, 19, 8, 15, 0 }, 
+										  { 10, 18, 7, 17, 0 }, 
+										  { 13, 16, 9, 14, 0 }, 
+										  { 12, 19, 8, 18, 0 } };
+
+	HungarianAlgorithm HungAlgo;
+	vector<int> assignment;
+
+	double cost = HungAlgo.Solve(costMatrix, assignment);
+
+	for (unsigned int x = 0; x < costMatrix.size(); x++)
+		std::cout << x << "," << assignment[x] << "\t";
+
+	std::cout << "\ncost: " << cost << std::endl;
+
+	return 0;
+}
